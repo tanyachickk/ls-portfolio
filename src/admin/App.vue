107c56a594afc6f0cc64e3bf668e390ebcc78321
tanyachickk <tatianakurochkina96@mail.ru>
2019-10-24 16:57:47 +0400

    add login form

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,36 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  .app
+    .login-page
+      .login-page__form
+        login-form
+    .inner-page
+      .inner-page__panel
+        header.header
+          .header__avatar
+            img.header__image(src="../images/content/author.jpg")
+          .header__content
+            .header__username Татьяна Курочкина
+            .header__title Панель администрирования
+            a.header__exit-link Выйти
+        .inner-page__navbar
+          .tabs
+            - var tabs = ['Обо мне', 'Работы', 'Отзывы'];
+              each tab, i in tabs
+                a.tabs__item(class=`${i === 0 ? 'tabs__item_active' : ''}`) #{tab}
+      .inner-page__content
 </template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+
+<style lang="postcss">
+@import './styles/main.pcss';
+</style>
---
+++ src/admin/components/LoginForm.vue
@@ -0,0 +1,124 @@
+<template lang="pug">
+  .login-form
+    .login-form__title Авторизация
+    button.login-form__close(@click="exitFromAdmin")
+      icon(name="remove")
+    form.login-form__body(@submit.prevent="signIn")
+      .login-form__control
+        basic-input(
+          v-model="name"
+          icon="user"
+          label="Логин"
+        )
+      .login-form__control
+        basic-input(
+          v-model="password"
+          icon="key"
+          label="Пароль"
+          type="password"
+        )
+      .login-form__button
+        basic-button(
+          type="submit"
+          :disabled="isLoading || !name.length || password.length < 4"
+        ) ОТПРАВИТЬ
+</template>
+
+<script>
+import axios from 'axios';
+import BasicInput from './BasicInput.vue';
+import BasicButton from './BasicButton.vue';
+import Icon from './Icon.vue';
+
+export default {
+  components: {
+    BasicInput,
+    BasicButton,
+    Icon,
+  },
+  data() {
+    return {
+      name: '',
+      password: '',
+      isLoading: false,
+    };
+  },
+  methods: {
+    async signIn() {
+      this.isLoading = true;
+      try {
+        await axios.post('https://webdev-api.loftschool.com/login', { name: this.name, password: this.password });
+        alert('Ok!');
+      } catch (e) {
+        alert(e.response.data.error);
+      }
+      this.password = '';
+      this.isLoading = false;
+    },
+    exitFromAdmin() {
+      location.href = 'https://tanyachickk.github.io/loftschool-course';
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+@import '../../styles/mixins.pcss';
+
+.login-form {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 563px;
+  width: 100vw;
+  padding: 60px 78px;
+  background-color: white;
+
+  @include phones {
+    height: 100vh;
+    padding: 30px;
+  }
+
+  &__close {
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    background: none;
+    fill: $text-color;
+    width: 20px;
+    height: 20px;
+    padding: 0;
+    outline: none;
+    transition: opacity 0.2s ease;
+    &:hover {
+      opacity: 0.3;
+    }
+  }
+
+  &__body {
+    @include phones {
+      align-self: stretch;
+    }
+  }
+
+  &__title {
+    color: $text-color;
+    font-size: 36px;
+    font-weight: 700;
+
+    @include phones {
+      font-size: 30px;
+    }
+  }
+
+  &__control {
+    margin-top: 60px;
+  }
+
+  &__button {
+    margin-top: 40px;
+  }
+}
+</style>

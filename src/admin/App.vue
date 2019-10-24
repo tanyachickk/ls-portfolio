a4dc2ac50c455dd1fb2ac9a770fe43c59eff33e0
tanyachickk <tatianakurochkina96@mail.ru>
2019-10-24 18:21:15 +0400

    add some styles

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -18,13 +18,24 @@
               each tab, i in tabs
                 a.tabs__item(class=`${i === 0 ? 'tabs__item_active' : ''}`) #{tab}
       .inner-page__content
+        .about-page
+          .about-page__header
+            h1.page-title Блок "Обо мне"
+            .about-page__add-group
+              button.basic-button.basic-button_small.basic-button_flat.basic-button_circle.basic-button_primary
+                .basic-button__icon-wrapper
+                  icon.basic-button__icon(name="plus")
+                .basic-button__text Добавить группу
+          .about-page__content
 </template>
 
 <script>
+import Icon from './components/Icon.vue';
 import LoginForm from './components/LoginForm.vue';
 
 export default {
   components: {
+    Icon,
     LoginForm
   }
 }

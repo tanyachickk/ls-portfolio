e338d0ab0af7e62394012ec642cda48bf6c3fcb1
Tatiana Kurochkina <tatianakurochkina96@mail.ru>
2019-10-31 15:35:22 +0400

    Add tests

--- src/admin/components/pages/Login.vue
+++ src/admin/components/pages/Login.vue
@@ -1,3 +1,24 @@
 <template lang="pug">
-  h1 Login
+  .login-page
+    .login-page__form
+      login-form
 </template>
+
+<script>
+export default {
+  components: {
+    LoginForm: () => import("../LoginForm.vue")
+  }
+};
+</script>
+
+<style lang="postcss" scoped>
+.login-page {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(45, 60, 78, 0.9);
+}
+</style>
